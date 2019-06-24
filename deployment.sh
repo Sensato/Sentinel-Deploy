ade1bbe553c25d30ee8e9a02ca8fec2a365a1ff6
Max Mason <1441513+neoglobalist@users.noreply.github.com>
2019-06-24 14:51:47 -0400

    more fixes, better pip install

--- deployment.sh
+++ deployment.sh
@@ -5,22 +5,32 @@ ENV="dev"
 ORG="hunterdon"
 MSSP="ctoc"
 
+TOKEN="??"
+
 sudo apt-get install software-properties-common
 sudo apt-add-repository universe
 sudo apt-add-repository multiverse
 
 sudo apt-get update
 
-sudo apt-get install git curl python python-pip
+sudo apt-get install git curl python 
+
+curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
+sudo python /tmp/get-pip.py
+pip install --user pipenv
+echo "PATH=$HOME/.local/bin:$PATH" >> ~/.profile
+source ~/.profile
+
 pip install azure-cosmos
 
 cd ~
-git clone http://e19af9ef1bd6aa60122f00bd25b0766879a4d4eb@github.com/Sensato/packer-nids.git
+git clone http://$TOKEN:x-oauth-basic@github.com/Sensato/packer-nids.git
 cd packer-nids
 git checkout appliance
 
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 az extension add --name azure-cli-iot-ext
 
-STR="Debug Message:\nBuilding Sentinel on environment $ENV, organization $ORG, msspid $MSSP."
+STR="Debug Message: Building Sentinel on environment $ENV, organization $ORG, msspid $MSSP."
+echo $STR
 python build_sensor.py env=$ENV orgid=$ORG msspid=$MSSP buildos=linux
