b4914fad85e4e1fc3917b063b552e48f12eb35bf
csharpe101 <51165738+csharpe101@users.noreply.github.com>
2019-08-07 14:23:18 -0400

    commented out logging, remove Sentinel-Deploy

--- bda-deployment.sh
+++ bda-deployment.sh
@@ -92,9 +92,13 @@ az extension add --name azure-cli-iot-ext
 STR="Debug Message: Building BDA on environment $ENV, organization $ORG, msspid $MSSP."
 echo $STR
 sudo mkdir -p ~/.debug
-sudo python build_bda.py env=$ENV orgid=$ORG msspid=$MSSP buildos=linux > ~/.debug/packer-bda.debug-$(date +"%s").log 2>&1
+sudo python build_bda.py env=$ENV orgid=$ORG msspid=$MSSP buildos=linux
+
+# Run the following command instead of the previous command to write build output to a file at ~/.debug
+# sudo python build_bda.py env=$ENV orgid=$ORG msspid=$MSSP buildos=linux > ~/.debug/packer-bda.debug-$(date +"%s").log 2>&1
 
 # Self Destruct
 # rm -rf ~/packer-bda
+# rm -rf ~/Sentinel-Deploy
 # sudo echo "exit 0" > /etc/rc.local # specific to BDA deployment, rc.local doesn't overwrite otherwise
 # sudo reboot
