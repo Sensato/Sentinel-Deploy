11ad3e2d2ccde452bcf7a739f4c5419a699cc355
RaymondILei <53795832+RaymondILei@users.noreply.github.com>
2019-09-13 13:54:08 -0400

    Update openvas10test.py

--- openvas10test.py
+++ openvas10test.py
@@ -274,11 +274,13 @@ def main():
     connection = UnixSocketConnection(path=path)
     transform = EtreeTransform()
     openvas_remote = OpenvasRemote(path, connection, transform)
-
+ 
+    #==================Comment the next 4 line with after you run this python script===================================
     id = openvas_remote.create_target('Raybox',['192.168.112.137'],'sensat0','!23$M@ddie')
     task_id = openvas_remote.create_task('OpenVasTestBox',  openvas_remote.get_full_and_fast_config_id(), id,  openvas_remote.get_default_openvas_scanner_id(), 'sensat0', '!23$M@ddie')
     report_id = openvas_remote.start_task(task_id,'sensat0','!23$M@ddie')
-    print(report_id)
+    print('use this id: ' + report_id + ' to get the report in a json format')
+    #==================UnComment next line after you run this python script==================
     #openvas_remote.get_report(report_id,['192.168.112.137'],'sensat0','!23$M@ddie')
 
 if __name__ == "__main__":
